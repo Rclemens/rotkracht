686be2e74195d9578a57588b43308f501a7b5696
Raymond Clemens <Raymond.Clemens@weareyou.com>
2022-12-28 23:11:22 +0100

    added CORS info

--- RotKrachtApi/TrainingFunction.cs
+++ RotKrachtApi/TrainingFunction.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using RotKrachtApi.Models;
 
 namespace RotKrachtApi
 {
@@ -39,17 +40,10 @@ namespace RotKrachtApi
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            string requestData = await new StreamReader(req.Body).ReadToEndAsync().ConfigureAwait(false);
+            var item  = JsonConvert.DeserializeObject<Training>(requestData);
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult(item);
         }
     }
 }
